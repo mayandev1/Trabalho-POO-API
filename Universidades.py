7622a56da6f53928ea2203c5675f9d2a6dc0af69
mayan.dev <mayangabriel654@gmail.com>
2026-05-08 11:20:33 -0300

    feat: Adição das funções Menu e Requisição_API

--- Universidades.py
+++ Universidades.py
@@ -1,10 +1,20 @@
 import requests
-#requisicao para a API
-url = "http://universities.hipolabs.com/search?country=Brazil"
-resposta = requests.get(url)
-print("Status: ", resposta.status_code)
 
-#converte para dicionario
-dados = resposta.json()
+def requisicao_API(dados):
+    # requisicao para a API
+    url = "https://universities.hipolabs.com/search?country=Brazil"
+    resposta = requests.get(url)
+    print("Status: ", resposta.status_code)
 
+    # converte para dicionario
+    dados = resposta.json()
+    return dados
 
+def menu():
+    print("===== MENU API DE UNIVERSIDADES =====")
+    print("1. Listar Dados")
+    print("2. Buscar um Item")
+    print("3. Detalhes")
+    print("4. Filtrar Dados")
+    print("5. Ordenar Dados")
+    print("6. Verificar Item")
